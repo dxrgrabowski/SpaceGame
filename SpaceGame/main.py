25f6ffcc5c7d5688ea69fd1069333eb635890075
Dawid Grabowski <dg301701@student.polsl.pl>
2022-01-30 16:59:43 +0100

    Porzadek w funkcjach

--- SpaceGame/main.py
+++ SpaceGame/main.py
@@ -7,9 +7,15 @@ width,height=800,1000
 win=pygame.display.set_mode((width,height))
 pygame.display.set_caption("Space by @Dexor")
 mainFont = pygame.font.SysFont("cambria", 50)
+
+#IMG load and transform
 BG=pygame.image.load(
     os.path.join('SpaceGame\Assets', 'background_blue_1.png'))
-FPS=120
+purpleSmallPlayerIMG=pygame.image.load(
+    os.path.join('SpaceGame\Assets', 'purple_small_player.png'))
+purpleSmallPlayer=pygame.transform.scale(purpleSmallPlayerIMG,(48,48))
+mainMenuIMG=pygame.image.load(
+    os.path.join('SpaceGame\Assets', 'Skyspace_logo_white.png'))
 
 #Objects
 class Bullet:
@@ -40,11 +46,11 @@ class Ship:
             self.cooldownC=1
         elif self.cooldownC>0:
             self.cooldownC+=1
-    def shoot(self):
-        if self.cooldownC==0:
-            bullet=Bullet(x,y,self.bulletIMG)
-            self.bullets.append(bullet)
-            self.cooldownC=1
+    #def shoot(self):
+        #if self.cooldownC==0:
+           # bullet=Bullet(x,y,self.bulletIMG)
+            #self.bullets.append(bullet)
+            #self.cooldownC=1
     
     def get_width(self):
         return self.shipIMG.get_width()
@@ -58,54 +64,63 @@ class Player(Ship):
         self.mask=pygame.mask.from_surface(self.shipIMG)
         self.max_health=health
 
-purpleSmallPlayerIMG=pygame.image.load(
-    os.path.join('SpaceGame\Assets', 'purple_small_player.png'))
-purpleSmallPlayer=pygame.transform.scale(purpleSmallPlayerIMG,(48,48))
 
-
-#Window draw
-def windowDraw():
-    player=Player(400,500,100)
-    while True:
-        pygame.mouse.set_visible(0)
-        win.fill("#1c1e1f")
-        player.draw(win)
-        win.blit(purpleSmallPlayer,(pygame.mouse.get_pos()))
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        event = pygame.event.wait()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                mainMenu()
-        pygame.display.update() 
-    
-#Menu
-def get_font(size): # Returns Press-Start-2P in the desired size
-    return pygame.font.Font("SpaceGame/assets/font.ttf", size)
-def mainMenu():
-    clock=pygame.time.Clock()
+def main():
     run=True
+    FPS=120
+    clock=pygame.time.Clock()
+    pygame.mouse.set_visible(0)
+    
+    def windowDraw():
+        player=Player(400,500,100)
+        while True:
+            pygame.mouse.set_visible(0)
+            win.fill("#1c1e1f")
+            player.draw(win)
+            win.blit(purpleSmallPlayer,(pygame.mouse.get_pos()))
+        
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            event = pygame.event.wait()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    mainMenu()
+            pygame.display.update() 
+    
     while run:
         clock.tick(FPS)
+        windowDraw()
+
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()    
+
+    
+#Menu font
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("SpaceGame/assets/font.ttf", size)
+
+#Main Menu
+def mainMenu():
+    run=True
+    pygame.mouse.set_visible(1)
+    while run:
         win.blit(BG,(0,0))
-        pygame.mouse.set_visible(1)
         menuPos=pygame.mouse.get_pos()
-        mainMenuIMG=pygame.image.load(
-        os.path.join('SpaceGame\Assets', 'Skyspace_logo_white.png'))
         menuTXT=get_font(100).render("MAIN MENU", True, "#b68f40")
-        menuRect=menuTXT.get_rect(center=(640,100))
-        playButton = Button(image=pygame.image.load("SpaceGame/assets/Play Rect.png"), pos=(640, 320), 
+        menuRect=menuTXT.get_rect(center=(450,100))
+        playButton = Button(image=pygame.image.load("SpaceGame/assets/Play Rect.png"), pos=(400, 320), 
                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        quitButton = Button(image=pygame.image.load("SpaceGame/assets/Quit Rect.png"), pos=(640, 480), 
+        quitButton = Button(image=pygame.image.load("SpaceGame/assets/Quit Rect.png"), pos=(400, 480), 
                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
         win.blit(menuTXT,menuRect)
 
         for button in [playButton,quitButton]:
             button.changeColor(menuPos)
             button.update(win)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run=False
@@ -113,7 +128,7 @@ def mainMenu():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if playButton.checkForInput(menuPos):
-                    windowDraw()
+                    main()
                 if quitButton.checkForInput(menuPos):
                     run=False
                     pygame.quit()
